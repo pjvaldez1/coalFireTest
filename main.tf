c680869895543963c1d7711c2b616d6e1db37dd9
pjvaldez <pjvaldez@gmail.com>
2023-08-15 19:53:22 -0700

    add git ignore

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
  
@@ -6,10 +21,10 @@ resource "aws_vpc" "main" {
  }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+#resource "aws_route#53_record" "www" {
+#  zone_id = aws_route53_zone.primary.zone_id
+#  name    = "www.example.com"
+#  type    = "A"
+#  ttl     = 300
+#  records = [aws_eip.lb.public_ip]
+#}
