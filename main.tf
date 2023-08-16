b00f0b35342b3b82f640a342c9960298d4745a89
pjvaldez <pjvaldez@gmail.com>
2023-08-15 20:39:16 -0700

    adding github Actions

--- main.tf
+++ main.tf
@@ -13,13 +13,23 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "prodVPC" {
+ cidr_block = "10.1.0.0/16"
  
  tags = {
-   Name = "Project VPC"
+   Name = "Prod-VPC"
  }
 }
+
+resource "aws_subnet" "PublicSubnet1" {
+  vpc_id = "${aws_vpc.prodVPC.id}"
+  cidr_block = "10.1.0.0/24"
+ 
+ tags = {
+   Name = "PublicSub1"
+ }
+}
+
 
 #resource "aws_route#53_record" "www" {
 #  zone_id = aws_route53_zone.primary.zone_id
